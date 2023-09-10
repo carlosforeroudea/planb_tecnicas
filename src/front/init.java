3f5442ed1fdc58f21399c4cb37f149c94120d441
carlosforeroudea <carlos.forero@udea.edu.co>
2023-09-10 18:03:51 -0500

    new changes

--- src/front/init.java
+++ src/front/init.java
@@ -6,6 +6,8 @@ public class init {
     public static void main(String[] args){
         bank_process b = new bank_process();
         Home inicio = new Home(b);
+
+
         inicio.setVisible(true);
         inicio.setSize(500,500);
 
