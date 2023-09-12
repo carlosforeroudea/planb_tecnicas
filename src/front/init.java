2f705b715a6b21903aedfec59f4743b4f4563365
carlosforeroudea <carlos.forero@udea.edu.co>
2023-09-11 21:36:15 -0500

    new forms

--- src/front/init.java
+++ src/front/init.java
@@ -5,11 +5,8 @@ import process.bank_process;
 public class init {
     public static void main(String[] args){
         bank_process b = new bank_process();
-        Home inicio = new Home(b);
-
-
-        inicio.setVisible(true);
-        inicio.setSize(500,500);
+        new Login().setVisible(true);
 
     }
+
 }
